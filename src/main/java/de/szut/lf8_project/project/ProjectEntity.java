91ae3ffad7fc2403ddf378ce0a4bad13ed41ff3d
Robin Lübbert <robin.luebbert@heptacom.de>
2022-11-03 11:32:12 +0100

    Rename project table

--- src/main/java/de/szut/lf8_project/project/ProjectEntity.java
+++ src/main/java/de/szut/lf8_project/project/ProjectEntity.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @Data
-@Table(name="products")
+@Table(name="project")
 public class ProjectEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
